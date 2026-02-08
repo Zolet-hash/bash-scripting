b2ce0c30888b047a8ee505c1b084218cd151e107
zolet-hash <emmanuelsulwe@gmail.com>
2026-02-08 13:23:56 +0300

    remove files and factor my dir structure

--- tutoring/overview/input.sh
+++
@@ -1,5 +0,0 @@
-#!bin/bash
-echo "What is your name?"
-read user_name
-
-echo "Hello, $user_name ! welcome to shell scripting"
--- tutoring/overview/variables.sh
+++
@@ -1,6 +0,0 @@
-#!bin/bash
-name="Parrot User"
-current_date=$(date)
-
-echo "Hello, $name!"
-echo "Today is: ${current_date}"
