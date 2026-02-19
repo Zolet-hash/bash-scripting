e413f8b9776b7757195e1d63b59501ac07438de2
Zolet-hash <emmanuelsulwe@gmail.com>
2026-02-19 19:40:17 +0000

    Update email_notification.sh

--- RTSS/email_notification.sh
+++ RTSS/email_notification.sh
@@ -3,3 +3,4 @@ subject="Alert: High cpu usage"
 body="Cpu usage exceeded 90"
 recipient="emmanuelsulwe@gmail.com"
 echo "$body" | mail -s "$subject" "$recipient"
+#incomplete to add more validations
